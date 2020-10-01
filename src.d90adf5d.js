37682e9ce4f872db25e38bd889125c4ecc2876a5
tommypratama <tommypratama@users.noreply.github.com>
2020-10-01 17:05:35 +0000

    deploy: 095a803d2e206d0dff4ae240b2cb1041ff4c1374

--- src.d90adf5d.js
+++ src.d90adf5d.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./styles.css"),console.log("hello world!");
 },{"./styles.css":"Tnu0"}]},{},["Focm"], null)
-//# sourceMappingURL=https://tommypratama.github.io/start-parcel-tailwindcss/src.d90adf5d.js.map
+//# sourceMappingURL=https://tommypratama.github.io/easy-bank-landing-page/src.d90adf5d.js.map
